7ab07fcf314b02777d85848187afeb57de2c743c
summer <levantung14112002@gmail.com>
2023-05-25 23:43:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "../frontend/public/upload");
+        cb(null, "../client/public/upload");
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname);
